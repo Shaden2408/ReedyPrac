e3272c1f1d5b7280fca3adb7fa3c55961e6f56d3
Shaden <>
2021-01-24 11:07:15 +0100

    first commit

---
+++ winterklausur 16/a5.cpp
@@ -0,0 +1,84 @@
+// Aufgabe 5 ( 10 Punkte )
+// Die untenstehende Datei a5.dat listet einige US-Präsidenten auf.
+// Die Datei enthält - in dieser Reihenfolge - :
+// Name, Jahre des Amtsantritts, Jahr des Ausscheidens aus dem Amt
+// Washington 1789 1797
+// Roosevelt 1933 1945
+// Obama 2009 2017
+// Kennedy 1961 1963
+// Taylor 1849 1850
+// Clinton 1993 2001
+// Fillmore 1850 1853
+// Nixon 1969 1974
+// Lincoln 1861 1865
+// Entwickeln Sie für das untenstehende Programm a5.cpp (siehe nächste Seite) die
+// Sortierfunktion vergleich, die diese Daten aufsteigend nach der Amtszeit (=EndjahrAnfangsjahr der Präsidentschaft) sortiert.
+// Haben zwei Präsidenten die gleiche Amtszeit, so sollen diese Präsidenten alphabetisch
+// geordnet werden.
+// Nach Ablauf von a5.exe soll also die Datei also wie folgt sortiert am Bildschirm erscheinen:
+// Taylor 1849 1850
+// Kennedy 1961 1963
+// Fillmore 1850 1853
+// Lincoln 1861 1865
+// Nixon 1969 1974
+// Clinton 1993 2001
+// Obama 2009 2017
+// Washington 1789 1797
+// Roosevelt 1933 1945
+// Tipp: Zwei Strings s1 und s2 sind dann alphabetisch sortiert, wenn
+// gilt s1 < s2.
+
+/* a5.cpp */
+#include <iostream> // ein- und ausgabe auf die console
+#include <fstream> // ein- und auslesen von datein
+#include <string> // arbeiten mit strings
+#include <algorithm> // qsort
+
+using namespace std;
+
+struct president
+{
+    string name;
+    int first;
+    int last;
+}usa[9];
+
+int vergleich(const void *av, const void *bv);
+
+int main(void)
+{
+    int n = 0;
+    ifstream eingabe_datei;
+    eingabe_datei.open("a5.dat");
+    if (!eingabe_datei)
+    {
+        cout << "Fehler beim Oeffnen der Datei !";
+        exit(-1);
+    }
+    while (eingabe_datei >> usa[n].name >> usa[n].first >>
+           usa[n].last)
+        n++;
+    qsort(usa, n, sizeof(struct president), vergleich);
+    for (int i = 0; i < n; i++)
+        cout << usa[i].name << " " << usa[i].first << " " << usa[i].last << endl;
+
+    return 0;
+}
+
+int vergleich(const void *av, const void *bv)
+{
+    struct president* p1 = (struct president*)av;
+    struct president* p2 = (struct president*)bv;
+
+    int zeit1 = p1->last -p1->first;
+    int zeit2 = p2->last -p2->first;
+
+    if (zeit1 == zeit2)
+    {
+        return p1->name.compare(p2->name);
+    }
+    else
+    {
+        return zeit1 - zeit2;
+    }
+}
---
+++ winterklausur 17/a4.cpp
@@ -0,0 +1,22 @@
+// Aufgabe 4 (10 Punkte)
+// Schreiben Sie ein C++-Programm a4.cpp mit zugehöriger ausführbarer Datei a4.exe.
+// An a4.exe werden beim Aufruf vom Betriebssystem aus (z.B. Windows Eingabeaufforderung)
+// beliebig viele zweistellige int-Zahlen (jeweils als C-String) übergeben.
+// Das Programm soll aus den eingegebenen Zahlen die Anzahl der geraden Zahlen, sowie
+// die Anzahl aller Zahlen, die mit der Ziffer 5 enden herausfinden und ausgeben.
+// Beispiel: a4.exe 17 95 15 12 40 35 77
+// ergibt als Bildschirmausgabe
+// Anzahl der geraden Zahlen: 2
+// Anzahl der Zahlen mit Endziffer 5: 3
+// Hinweis: Sie können die Funktion int atoi (char* s) verwenden.
+// Diese liefert als Rückgabewert die dem C-String s entsprechende Zahl.
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+    return 0;
+}
